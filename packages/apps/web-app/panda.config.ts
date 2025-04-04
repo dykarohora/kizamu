df856783c0e7e7b089293501f70e34236d9280fc
dykarohora <7d.yama@gmail.com>
2025-04-05 00:15:06 +0900

    feat(web-app): update color palette in panda.config.ts to use sage and teal

--- packages/apps/web-app/panda.config.ts
+++ packages/apps/web-app/panda.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from '@pandacss/dev'
 import { createPreset } from '@park-ui/panda-preset'
-import orange from '@park-ui/panda-preset/colors/orange'
-import sand from '@park-ui/panda-preset/colors/sand'
+import sage from '@park-ui/panda-preset/colors/sage'
+import teal from '@park-ui/panda-preset/colors/teal'
 
 export default defineConfig({
   preflight: true,
-  presets: [createPreset({ accentColor: orange, grayColor: sand, radius: 'sm' })],
+  presets: [createPreset({ accentColor: teal, grayColor: sage, radius: 'sm' })],
   include: ['./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}'],
   jsxFramework: 'react', // or 'solid' or 'vue'
   outdir: 'styled-system',
