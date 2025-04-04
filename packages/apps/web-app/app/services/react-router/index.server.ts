37937581bd8a2ddb7612fd72f1b3cf7952111473
dykarohora <7d.yama@gmail.com>
2025-04-05 00:17:31 +0900

    refactor(web-app): fix formatting of context classes in index.server.ts for consistency

--- packages/apps/web-app/app/services/react-router/index.server.ts
+++ packages/apps/web-app/app/services/react-router/index.server.ts
@@ -3,8 +3,8 @@ import type { ActionFunctionArgs, LoaderFunctionArgs } from 'react-router'
 
 type RequestPrams = Pick<LoaderFunctionArgs & ActionFunctionArgs, 'request' | 'params'>
 
-export class RequestContext extends Context.Tag('RequestContext')<RequestContext, RequestPrams>() { }
+export class RequestContext extends Context.Tag('RequestContext')<RequestContext, RequestPrams>() {}
 
-export class LoaderContext extends Context.Tag('LoaderContext')<LoaderContext, LoaderFunctionArgs>() { }
+export class LoaderContext extends Context.Tag('LoaderContext')<LoaderContext, LoaderFunctionArgs>() {}
 
-export class ActionContext extends Context.Tag('ActionContext')<ActionContext, ActionFunctionArgs>() { }
+export class ActionContext extends Context.Tag('ActionContext')<ActionContext, ActionFunctionArgs>() {}
